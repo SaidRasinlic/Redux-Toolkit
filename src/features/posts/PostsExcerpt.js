be4ef166e8191712f12c12b3004cec552033aad5
Said Rasinlic <saidrasinlic.jr@gmail.com>
2023-06-12 17:32:58 +0200

    Add file

--- src/features/posts/PostsExcerpt.js
+++ src/features/posts/PostsExcerpt.js
@@ -1,25 +1,34 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { PropTypes } from 'prop-types';
+import { selectPostById } from './postsSlice';
 import PostAuthor from './PostAuthor';
 import TimeAgo from './TimeAgo';
 import ReactionButtons from './ReactionButtons';
 
-const PostsExcerpt = ({ post }) => (
-  <article>
-    <h3>{post.title}</h3>
-    <p>{post.body.substring(0, 100)}</p>
-    <p className="postCredit">
-      <Link to={`post/${post.id}`}>View Post</Link>
-      <PostAuthor userId={post.userId} />
-      <TimeAgo timestamp={post.date} />
-    </p>
-    <ReactionButtons post={post} />
-  </article>
-);
+// eslint-disable-next-line import/no-mutable-exports
+const PostsExcerpt = ({ postId }) => {
+  const post = useSelector((state) => selectPostById(state, postId));
+
+  return (
+    <article>
+      <h3>{post.title}</h3>
+      <p>{post.body.substring(0, 100)}</p>
+      <p className="postCredit">
+        <Link to={`post/${post.id}`}>View Post</Link>
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+      </p>
+      <ReactionButtons post={post} />
+    </article>
+  );
+};
+
+// PostsExcerpt = React.memo(PostsExcerpt);
 
 PostsExcerpt.propTypes = {
   // PropTypes.shape({}).isRequired
-  post: PropTypes.oneOfType([PropTypes.object]).isRequired,
+  postId: PropTypes.oneOfType([PropTypes.number]).isRequired,
 };
 
 export default PostsExcerpt;
